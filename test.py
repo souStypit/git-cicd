de195d79fb3b5b0542ca20da4f90cc553da90a34
Ilya <s4venkov1lya@gmail.com>
2024-12-05 19:30:17 +0300

    modified test

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 from main import factorial
 import math
 
+def tests():
+    try:
+        assert math.factorial(10) == factorial(10)
+        assert math.factorial(12) == factorial(12)
+        assert math.factorial(15) == factorial(15)
+    except AssertionError as ae:
+        return 1
+    return 0
 def main():
-    x = 10
-    return math.factorial(x) != factorial(x)
+    return tests()
     
 if __name__ == '__main__':
     exit(main())
