73366fbff98b30635fbc66491dde2b0ed53b8eba
Ilya <s4venkov1lya@gmail.com>
2024-12-05 20:09:21 +0300

    new funcs

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from numpy import exp, sqrt, pi
+
 def factorial(n: int):
     if n < 1:
         return 1
     else:
         return n * factorial(n - 1)
+    
+def del_nan_vals(darray: list):
+    return [0 if el is None else el for el in darray]
+
+def gauss(x: float, loc=0, scale=1):
+    nu = loc
+    sigma = scale
+    return exp(-((x - nu)**2)/(2 * sigma ** 2)) / (sqrt(2 * pi * sigma ** 2))
+    
+
+if __name__ == '__main__':
+    print(del_nan_vals([10, 20, 30, None]) == [10, 20, 30, 0])
+    print(round(gauss(0.09), 4) == 0.3973)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from main import factorial
+from main import factorial, del_nan_vals, gauss
 import math
 
 
@@ -7,6 +7,14 @@ def tests():
         assert math.factorial(10) == factorial(10)
         assert math.factorial(12) == factorial(12)
         assert math.factorial(15) == factorial(15)
+        
+        assert del_nan_vals([10, 20, 30, None]) == [10, 20, 30, 0]
+        assert del_nan_vals([10, None, 30, None]) == [10, 0, 30, 0]
+        assert del_nan_vals([None, 20, 30, None]) == [0, 20, 30, 0]
+        
+        assert round(gauss(0.09), 4) == 0.3973
+        assert round(gauss(0.19), 4) == 0.3918
+        assert round(gauss(0.29), 4) == 0.3825 
     except AssertionError as ae:
         print("Tests are failed")
         return 1
