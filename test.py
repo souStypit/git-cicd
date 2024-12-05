05a39ab60c6cb132557002241db410b01cd49343
Ilya <s4venkov1lya@gmail.com>
2024-12-05 19:43:56 +0300

    editting

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
 from main import factorial
 import math
 
+
 def tests():
     try:
         assert math.factorial(10) == factorial(10)
         assert math.factorial(12) == factorial(12)
         assert math.factorial(15) == factorial(15)
     except AssertionError as ae:
-        print('Tests are failed')
+        print("Tests are failed")
         return 1
-    print('Tests are completed')
+    print("Tests are completed")
     return 0
+
+
 def main():
     return tests()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     exit(main())
