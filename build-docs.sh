41da40412054c009cf7cd40bd84a2c1876907ca1
Ilya <s4venkov1lya@gmail.com>
2024-12-05 21:19:18 +0300

    1

--- build-docs.sh
+++ build-docs.sh
@@ -2,7 +2,7 @@
 lazydocs \
     --output-path="./docs/docstrings" \
     --overview-file="README.md" \
-    --src-base-url="https://github.com/<you github account>/<your repo>/blob/master/" \
+    --src-base-url="https://github.com/souStypit/git-cicd/main/" \
     <folder with source code>
 
 mkdocs build
