d5730af01e1cd89635dba845ba6f728723dfd435
Ilya Savenkov <96959440+souStypit@users.noreply.github.com>
2024-12-06 15:33:10 +0300

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'cicd'
+copyright = '2024, ilya'
+author = 'ilya'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.coverage'
+              ]
+
+sys.path.insert(0, os.path.abspath('../'))
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+auto_doc_default_options = {'autosummary': True}
---
+++ docs/modules.rst
@@ -0,0 +1,8 @@
+cicd
+====
+
+.. toctree::
+   :maxdepth: 4
+
+   main
+   test
