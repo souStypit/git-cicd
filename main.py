30954eb8308096fad1714773d84488f62023d7a7
Ilya Savenkov <96959440+souStypit@users.noreply.github.com>
2024-12-06 15:34:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 from numpy import exp, sqrt, pi
 
+
 def factorial(n: int):
+    """Counts factorial of 'n'
+
+    Args:
+        n (int): Inputed number
+
+    Returns:
+        int: Factorial of n
+    """
     if n < 1:
         return 1
     else:
         return n * factorial(n - 1)
-    
+
+
 def del_nan_vals(darray: list):
+    """Changes NaN values with 0
+
+    Args:
+        darray (list): Inputed list
+
+    Returns:
+        list: An array with changed NaN values
+    """
     return [0 if el is None else el for el in darray]
 
-def gauss(x: float, loc=0, scale=1):
-    nu = loc
-    sigma = scale
-    return exp(-((x - nu)**2)/(2 * sigma ** 2)) / (sqrt(2 * pi * sigma ** 2))
-    
 
-if __name__ == '__main__':
-    print(del_nan_vals([10, 20, 30, None]) == [10, 20, 30, 0])
-    print(round(gauss(0.09), 4) == 0.3973)
+def gauss(x: float, nu=0, sigma=1):
+    """Calculates gauss distribution in x
+
+    Args:
+        x (float): Point
+        nu (float): Mean
+        sigma (float): Sigma
+
+    Returns:
+        float: Gauss(x)
+    """
+    return exp(-((x - nu)**2)/(2 * sigma ** 2)) / (sqrt(2 * pi * sigma ** 2))
